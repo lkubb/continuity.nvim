1002ee0410c3337752247ea138688829ef68dd93
jeanluc <lkubb@protonmail.com>
2025-10-17 00:08:02 +0200

    feat: Save/restore all quicklists + their config

--- lua/continuity/extensions/quickfix.lua
+++ lua/continuity/extensions/quickfix.lua
@@ -2,26 +2,54 @@
 local M = {}
 
 M.on_save = function()
-  return vim.tbl_map(function(item)
-    return {
-      filename = item.bufnr and vim.api.nvim_buf_get_name(item.bufnr),
-      module = item.module,
-      lnum = item.lnum,
-      end_lnum = item.end_lnum,
-      col = item.col,
-      end_col = item.end_col,
-      vcol = item.vcol,
-      nr = item.nr,
-      pattern = item.pattern,
-      text = item.text,
-      type = item.type,
-      valid = item.valid,
+  local cnt = vim.fn.getqflist({ nr = "$" }).nr
+  local ret = {}
+  if cnt <= 0 then
+    return ret
+  end
+  local pos = vim.fn.getqflist({ nr = 0 }).nr
+  for i = 1, cnt do
+    local qflist = vim.fn.getqflist({ nr = i, all = true })
+    ret[#ret + 1] = {
+      idx = qflist.idx,
+      title = qflist.title,
+      efm = qflist.efm,
+      quickfixtextfunc = qflist.quickfixtextfunc,
+      items = vim.tbl_map(function(item)
+        return {
+          filename = item.bufnr and vim.api.nvim_buf_get_name(item.bufnr),
+          module = item.module,
+          lnum = item.lnum,
+          end_lnum = item.end_lnum,
+          col = item.col,
+          end_col = item.end_col,
+          vcol = item.vcol,
+          nr = item.nr,
+          pattern = item.pattern,
+          text = item.text,
+          type = item.type,
+          valid = item.valid,
+        }
+      end, qflist.items),
     }
-  end, vim.fn.getqflist())
+  end
+  return { ret, pos }
 end
 
 M.on_pre_load = function(data)
-  vim.fn.setqflist(data)
+  local lists, pos = data[1], data[2]
+  if not (lists and pos) then
+    if not data.filename then
+      return
+    end
+    -- migration
+    lists, pos = { items = data }, 1
+  end
+  vim.fn.setqflist({}, "f") -- ensure lists are always cleared
+  vim.iter(lists):each(function(qflist)
+    vim.fn.setqflist({}, " ", qflist)
+  end)
+  vim.cmd.chistory({ count = pos })
 end
 
 ---@diagnostic disable-next-line: unused
